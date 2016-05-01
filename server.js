849eda87a84498f7c8dfa08c7ab8b6cfed77e99e
Jared Mata <flaco777.jm@gmail.com>
2016-04-30 19:53:38 -0700

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require("body-parser");
+var _ = require("underscore");
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -13,20 +14,13 @@ app.get('/', function (req, res) {
 
 // GET /todos
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	res.json(todos)
 });
 
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoID =  parseInt(req.params.id, 10);
-	var matchedTodo;
-
-	todos.forEach(function (todo) {
-		if (todoID === todo.id) {
-			matchedTodo = todo;
-		}
-	});
-	
+	var matchedTodo = _.findWhere(todos, {id: todoID});	
 	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
@@ -37,7 +31,15 @@ app.get('/todos/:id', function (req, res) {
 
 //POST /todos
 app.post("/todos", function (req, res) { 
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed'); // use _.pick to only pick description and completed
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+	// set body.description to the trimmed value
+
+
  	body.id = todoNextID++;
 	todos.push(body);
 	res.json(todos);
