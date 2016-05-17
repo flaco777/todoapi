c399f402d7b868c91b9a306c8a38c94f079ad8d1
Jared Mata <flaco777.jm@gmail.com>
2016-05-16 19:39:39 -0700

    add var-playground.js

---
+++ var-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+// 	name: 'Jared',
+// 	age: 15
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Jared',
+// 	// 	age: 24
+// 	// };
+// 	// return (obj);
+// 	obj.age = 24;
+// }
+
+
+// updatePerson(person);
+// console.log(person);
+
+
+var array = [15, 27];
+
+function updateArray (arr) {
+	arr.push(18);
+	debugger;
+}
+
+updateArray(array);
+console.log(array);
